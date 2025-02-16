ba9c8b4b5b6e1ae13972aac1606137cc9f89983b
mastermind-fa <falam3399@gmail.com>
2025-02-16 18:45:45 +0600

    login button added

--- profile.js
+++ profile.js
@@ -26,7 +26,7 @@ async function fetchProfile() {
     displayProfile(data);
   } catch (error) {
     console.error("Error fetching profile data:", error);
-    alert("Failed to fetch profile data. Please try again later.");
+    // alert("Failed to fetch profile data. Please try again later.");
     
   }
 }
